37177abd2d7c9f8d23f7bc4462f71f844f95751d
Ashutosh Prasad <ashutoshprasad2427@gmail.com>
2024-07-07 09:49:31 -0700

    gsap basic, stagger,repeat.

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
-gsap.to(".box", {
-    x: 500,
-    y:200,
-    rotate: 360,
-    duration: 3,
-    delay:3
-    
+gsap.to(".box1", {
+  x: 1400,
+  y: 900,
+  duration: 10,
+  delay: 1,
+  rotate: 6520,
+
+  backgroundColor: "blue",
+  border: 5,
+  borderColor: "pink",
+  scale: 1,
+  repeat: -1,
+  yoyo: true,
+});
+gsap.to(".box2", {
+  x: 800,
+  duration: 20,
+  delay: 1,
+  rotate: 9990,
+  backgroundColor: "cyan",
+  border: 5,
+  borderColor: "pink",
+  scale: 1.1,
+  repeat: -1,
+  yoyo: true,
+  scale: 5,
+});
+gsap.to(".box3", {
+  x: 1000,
+  duration: 12,
+  delay: 1,
+  rotate: 11000,
+  backgroundColor: "yellow",
+  border: 5,
+  borderColor: "pink",
+  scale: 3,
+  repeat: -1,
+  yoyo: true,
+});
+
+gsap.to("h1",{
+    x:-200,
+    stagger:1,
+    duration:3,
+    repeat:-1,
+    yoyo:true
 })
