6faa4a261c03bc7dce5e34e1eeae04a1f131657f
Ashutosh Prasad <ashutoshprasad2427@gmail.com>
2024-07-18 10:28:03 +0530

    cursor now invert images on its lens

--- cursor.js
+++ cursor.js
@@ -6,17 +6,34 @@ main.addEventListener("mousemove", (props) => {
   gsap.to(cursors, {
     x: props.x,
     y: props.y,
+    background: "transparent",
   });
 });
 imageDiv.addEventListener("mouseenter", (data) => {
-  cursors.innerHTML = "zoom in";
   gsap.to(cursors, {
-    scale: 3,
+    css: {
+      scale: 5,
+      backgroundColor: "transparent",
+      backdropFilter: "invert(1)",
+      filter: "contrast(-500%)",
+      border: "none",
+      cursor: "none",
+    },
+    duration: 0.5,
+    ease: "back.out(1,7)",
   });
 });
 imageDiv.addEventListener("mouseleave", (data) => {
   cursors.innerHTML = "";
   gsap.to(cursors, {
-    scale: 1,
+    css: {
+      scale: 1,
+      backgroundColor: "transparent",
+      backdropFilter: "invert(0)",
+      filter: "contrast(-500%)",
+      border: "3px dashed rgb(249, 249, 249)",
+    },
+    duration: 0.5,
+    ease: "back.out(1,7)",
   });
 });
