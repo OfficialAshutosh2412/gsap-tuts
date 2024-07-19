ab0748d017a3f026b644b633d4eaacb75f97dba0
Ashutosh Prasad <ashutoshprasad2427@gmail.com>
2024-07-20 00:28:01 +0530

    navbar completed

--- navbar.js
+++ navbar.js
@@ -1,45 +1,9 @@
-// gsap.from(".lt", {
-//   duration: 2,
-//   x: -10,
-//   scale: 13,
-//   opacity: 0,
-//   rotate: 100,
-//   ease: "bounce.out",
-// });
-// gsap.from(".gt", {
-//   duration: 2,
-//   x: -100,
-//   scale: 5,
-//   opacity: 0,
-//   rotate: 100,
-//   ease: "bounce.out",
-//   color: "white",
-// });
-// gsap.from(".first", {
-//   duration: 1,
-//   x: -100,
-//   scale: 5,
-//   opacity: 0,
-//   color: "white",
-// });
-// gsap.from(".rest-name", {
-//   duration: 2,
-//   y: -200,
-//   opacity: 0,
-//   color: "white",
-// });
-// gsap.from(".surname", {
-//   duration: 2,
-//   y: -200,
-//   opacity: 0,
-//   color: "white",
-// });
 gsap.from("nav .brand div", {
-  stagger: 0.5,
+  stagger: 0.2,
   y: -50,
   x: -500,
   opacity: 0,
-  duration: 2.5,
+  duration: 1.5,
   delay: 0,
   ease: "back.inOut",
   rotate: 50000,
@@ -47,10 +11,35 @@ gsap.from("nav .brand div", {
   scale: 20,
 });
 gsap.from(".menu", {
-  duration: 1,
-  x: -100,
-  y: -100,
+  duration: 2,
+  x: 900,
   opacity: 0,
   ease: "back.out",
   delay: 0.5,
+});
+var open = document.querySelector(".menu");
+var close = document.querySelector(".mobile-nav label");
+var tl = gsap.timeline();
+tl.pause();
+tl.to(".mobile-nav", {
+  right: "0%",
+  duration: 0.1,
+});
+tl.from(".mobile-nav div", {
+  x: 800,
+  duration: 0.5,
+  stagger: 0.2,
+  ease: "circ.out",
+});
+tl.from(".mobile-nav label", {
+  duration: 0.5,
+  y: -100,
+  ease: "circ.out",
+});
+
+open.addEventListener("click", function () {
+  tl.play();
+});
+close.addEventListener("click", function () {
+  tl.reverse();
 });
